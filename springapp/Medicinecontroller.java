99c8f02527920f2fb6dc821fc37b188aa9b195c2
Iamneo <techadmin@iamneo.ai>
2023-10-01 15:09:23 +0000

    Backup Commit

--- springapp/Medicinecontroller.java
+++ springapp/Medicinecontroller.java
@@ -2,7 +2,20 @@
 package com.springapp;
 import com.springapp.Medicine;
 import com.springapp.Medicineservice;
-import org.springframework.
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/medicines")
 public class Medicinecontroller {
-    
+    private final MedicineService medicineService;
+    @Autowired
+    public MedicineController(MedicineService medicineService){
+        this.medicineService=medicineService;
+    }
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine){
+        return medicineService.addMedicine(medicine);
+    }
+    @PutMapping("/{mediineId}")
+    public Medicine updateMedicine(@PathVariable int medicineId,@RequestBody Medicine updatedMedicine)
 }
