d6900789d9beedb3e5d164b9735296035f140bd3
Iamneo <techadmin@iamneo.ai>
2023-10-01 15:19:23 +0000

    Backup Commit

--- springapp/Medicineservice.java
+++ springapp/Medicineservice.java
@@ -1,6 +1,23 @@
 package springapp;
 import springapp.Medicine;
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+@Service
 public class Medicineservice {
-    
+    private final List<Medicine> medicines=new ArrayList<>();
+    private int nextMedicineId=1;
+    public boolean addMedicine(Medicine medicine){
+        medicine.setMedicineId(nextMedicineId++);
+        return medicines.add(medicine);
+    }
+    public Medicine updateMedicine(int medicineId,Medicine updatedMedicine){
+        for(Medicine medicine:medicines){
+            if(medicine.getMedicineId()==medicineId){
+                medicine.setMedicineName(updatedMedicine.getMedicineName());
+                medicine.setPrice(updatedMedicine.getPrice());
+                medicine.set
+            }
+        }
+    }
 }
